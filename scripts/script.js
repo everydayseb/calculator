db5fcf490bd8ef5033a9aec98db6fe8053a849da
Seb <everydayseb@pm.me>
2025-03-23 15:30:56 +0000

    Added operation selecting

--- scripts/script.js
+++ scripts/script.js
@@ -13,3 +13,20 @@ const multiply = function (a, b) {
 const divide = function (a, b) {
     return a / b;
 }
+
+const operate = function (a, b, operator) {
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+        default:
+            console.log("ERR: UNKNOWN OPERATION");
+    }
+}
+
+let a, b, operator;

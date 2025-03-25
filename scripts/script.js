6348b23fb41ab70b8af303006360f2a3efff9f5a
Seb <everydayseb@pm.me>
2025-03-25 20:46:37 +0000

    Prevent division by zero

--- scripts/script.js
+++ scripts/script.js
@@ -23,10 +23,21 @@ const operate = function (a, b, operator) {
         case '*':
             return multiply(a, b);
         case '/':
-            return divide(a, b);
+            if (b === 0) {
+                console.log("Tried to divide by 0. Please don't.");
+                return;
+            } else {
+                return divide(a, b);
+            }
         default:
             console.log("ERR: UNKNOWN OPERATION");
     }
 }
 
 let a, b, operator;
+
+// Create the functions that populate the display when you click the digit 
+// buttons. You should store the content of the display (the number) in a 
+// variable for use in the next step.
+
+// button events
